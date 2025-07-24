4d9a8c873017ef83fe856ba4319bcabc79083cfc
Callypige <sophie.nourry58@outlook.fr>
2025-07-24 20:55:02 +0200

    Fix MongoDB environment variable name

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGO;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-    throw new Error (" please define mongo environment variable")
+    throw new Error("Please define MONGODB_URI environment variable")
 }
 
 async function connectToDatabase() {
